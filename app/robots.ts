5c4fccded1a598270dc6c14b130117c183d007e7
Mrabet123 <mrabetyassuo@gmail.com>
2026-08-02 18:08:44 +0100

    fixing the website urll

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://accanto-care.com/sitemap.xml",
+    sitemap: "https://www.accanto-care.com/sitemap.xml",
   };
 }
